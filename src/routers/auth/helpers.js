f587898fe17e487884db285c51aa5a1fccebc3f3
Andraž Cuderman <andraz.cuderman@globalid.net>
2019-12-29 22:28:32 +0100

    parameter name fix

--- src/routers/auth/helpers.js
+++ src/routers/auth/helpers.js
@@ -6,5 +6,6 @@ export async function verifyToken(token) {
 }
 
 export async function getYoutubeAccessToken(access_token) {
+  console.log(`${GOOGLE_OAUTH_BASE_URI}tokeninfo?access_token=${access_token}`)
     return axios.get(`${GOOGLE_OAUTH_BASE_URI}tokeninfo?access_token=${access_token}`)
 }
--- src/routers/auth/index.js
+++ src/routers/auth/index.js
@@ -7,8 +7,7 @@ import { protectedRoute } from '../../middleware/protected';
 const router = Router()
 
 router.post('/token', async (req, res, next) => {
-    console.log('started executing token sctipt')
-    const { id_token, youtube_api_access_token } = req.body
+    const { id_token, youtube_access_token } = req.body
     try {
         await verifyToken(id_token)
     } catch(e) {
@@ -22,11 +21,11 @@ router.post('/token', async (req, res, next) => {
     let youtubeTokenData;
     try {
         youtubeTokenData = 
-            await getYoutubeAccessToken(youtube_api_access_token)
+            await getYoutubeAccessToken(youtube_access_token)
     } catch(e) {
         console.log(e)
         res.status(400).json({
-            err_code: 'INVALID_API_TOKEN' 
+            err_code: 'INVALID_API_TOKEN',
         })
         return
     }
@@ -37,7 +36,7 @@ router.post('/token', async (req, res, next) => {
     } */
 
     const access_token = jwt.sign({ 
-        youtube_api: youtube_api_access_token,
+        youtube_api: youtube_access_token,
         role: 'admin',
         exp: parseInt(youtubeTokenData.data.exp),
         iss: 'famnit-tutorials'
